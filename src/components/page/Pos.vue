6aabc0bfecf736dd37a039cff3b684d951346111
yami <yami19931023@163.com>
2019-08-24 20:23:09 +0800

    第一次提交

---
+++ src/components/page/Pos.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="pos">
+    WelCome To Pos
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'pos',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+
+</style>
