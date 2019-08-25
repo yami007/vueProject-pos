1d86d94942318c0d6149195f5adc3d7309f44e18
yami <yami19931023@163.com>
2019-08-25 15:36:21 +0800

    简单业务逻辑，axios

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -2,23 +2,24 @@
   <div class="pos">
     <el-row>
       <el-col :span="7" class="pos-order" id="order-list">
-        <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tabs>
           <el-tab-pane label="点餐" name="first" >
             <el-table :data="tableData" border style="width: 100%">
               <el-table-column prop="goodsName" label="商品名称" ></el-table-column>
               <el-table-column prop="count" label="数量" width="50"></el-table-column>
               <el-table-column prop="price" label="金额" width="70"></el-table-column>
               <el-table-column label="操作" width="100" fixed="right">
-                <template>
-                  <el-button type="text" size="small">添加</el-button>
-                  <el-button type="text" size="small">删除</el-button>
+                <template scope="scope">
+                  <el-button type="text" size="small" @click="addtableData(scope.row)">添加</el-button>
+                  <el-button type="text" size="small" @click="delSingleGoods(scope.row)">删除</el-button>
                 </template>
               </el-table-column>
             </el-table>
+            <div>总数量：{{totalCount}}  总金额{{totalMoney}}</div>
             <div class="div-btn">
               <el-button type="danger">挂单</el-button>
-              <el-button type="warning">删除</el-button>
-              <el-button type="success">结账</el-button>
+              <el-button type="warning" @click="delAllGoods()">删除</el-button>
+              <el-button type="success" @click="checkout()">结账</el-button>
             </div>
           </el-tab-pane>
           <el-tab-pane label="挂单" name="second">挂单</el-tab-pane>
@@ -30,8 +31,8 @@
           <div class="title">常用商品</div>
           <div class="often-goods-list">
             <ul>
-              <li v-for="goods in oftenGoods">
-                <span>{{goods.goodsName}}</span>
+              <li v-for="goods in oftenGoods" @click= "addtableData(goods)">
+                <span >{{goods.goodsName}}</span>
                 <span class="o-price">￥{{goods.price}}元</span>
               </li>
             </ul>
@@ -43,7 +44,7 @@
             <el-tab-pane label="汉堡" >
               <div>
                 <ul class='cookList'>
-                  <li v-for="goods in type0Goods">
+                  <li v-for="goods in type0Goods" @click= "addtableData(goods)">
                     <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
                     <span class="foodName">{{goods.goodsName}}</span>
                     <span class="foodPrice">￥{{goods.price}}元</span>
@@ -51,9 +52,39 @@
                 </ul>
               </div>
             </el-tab-pane>
-            <el-tab-pane label="小食" >小食</el-tab-pane>
-            <el-tab-pane label="饮料" >饮料</el-tab-pane>
-            <el-tab-pane label="套餐" >套餐</el-tab-pane>
+            <el-tab-pane label="小食" >
+              <div>
+                <ul class='cookList'>
+                  <li v-for="goods in type1Goods" @click= "addtableData(goods)">
+                    <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
+                    <span class="foodName">{{goods.goodsName}}</span>
+                    <span class="foodPrice">￥{{goods.price}}元</span>
+                  </li>
+                </ul>
+              </div>
+            </el-tab-pane>
+            <el-tab-pane label="饮料" >
+              <div>
+                <ul class='cookList'>
+                  <li v-for="goods in type2Goods" @click= "addtableData(goods)">
+                    <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
+                    <span class="foodName">{{goods.goodsName}}</span>
+                    <span class="foodPrice">￥{{goods.price}}元</span>
+                  </li>
+                </ul>
+              </div>
+            </el-tab-pane>
+            <el-tab-pane label="套餐" >
+              <div>
+                <ul class='cookList'>
+                  <li v-for="goods in type3Goods" @click= "addtableData(goods)">
+                    <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
+                    <span class="foodName">{{goods.goodsName}}</span>
+                    <span class="foodPrice">￥{{goods.price}}元</span>
+                  </li>
+                </ul>
+              </div>
+            </el-tab-pane>
           </el-tabs>
         </div>
       </el-col>
@@ -62,134 +93,111 @@
 </template>
 
 <script>
+  import axios from 'axios';
   export default {
     name: 'pos',
     data () {
       return {
-        tableData: [{
-          goodsName: '可口可乐',
-          price: 8,
-          count:1
-        }, {
-
-          goodsName: '香辣鸡腿堡',
-          price: 15,
-          count:1
-        }, {
-
-          goodsName: '爱心薯条',
-          price: 8,
-          count:1
-        }, {
-
-          goodsName: '甜筒',
-          price: 8,
-          count:1
-        }],
+        tableData: [],
         oftenGoods:[
-          {
-            goodsId:1,
-            goodsName:'香辣鸡腿堡',
-            price:18
-          }, {
-            goodsId:2,
-            goodsName:'田园鸡腿堡',
-            price:15
-          }, {
-            goodsId:3,
-            goodsName:'和风汉堡',
-            price:15
-          }, {
-            goodsId:4,
-            goodsName:'快乐全家桶',
-            price:80
-          }, {
-            goodsId:5,
-            goodsName:'脆皮炸鸡腿',
-            price:10
-          }, {
-            goodsId:6,
-            goodsName:'魔法鸡块',
-            price:20
-          }, {
-            goodsId:7,
-            goodsName:'可乐大杯',
-            price:10
-          }, {
-            goodsId:8,
-            goodsName:'雪顶咖啡',
-            price:18
-          }, {
-            goodsId:9,
-            goodsName:'大块鸡米花',
-            price:15
-          }, {
-            goodsId:20,
-            goodsName:'香脆鸡柳',
-            price:17
-          }
         ],
         type0Goods:[
-          {
-            goodsId:1,
-            goodsImg:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566672597068&di=100fe0624bc594ae04b23bb7ab540fec&imgtype=0&src=http%3A%2F%2Fpic.youngt.com%2Fstatic%2Fteam%2F20150528%2F20150528153337_47256.jpg",
-            goodsName:'香辣鸡腿堡',
-            price:18
-          }, {
-            goodsId:2,
-            goodsImg:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566672597068&di=100fe0624bc594ae04b23bb7ab540fec&imgtype=0&src=http%3A%2F%2Fpic.youngt.com%2Fstatic%2Fteam%2F20150528%2F20150528153337_47256.jpg",
-            goodsName:'田园鸡腿堡',
-            price:15
-          }, {
-            goodsId:3,
-            goodsImg:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566672597068&di=100fe0624bc594ae04b23bb7ab540fec&imgtype=0&src=http%3A%2F%2Fpic.youngt.com%2Fstatic%2Fteam%2F20150528%2F20150528153337_47256.jpg",
-            goodsName:'和风汉堡',
-            price:15
-          }, {
-            goodsId:4,
-            goodsImg:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566672597068&di=100fe0624bc594ae04b23bb7ab540fec&imgtype=0&src=http%3A%2F%2Fpic.youngt.com%2Fstatic%2Fteam%2F20150528%2F20150528153337_47256.jpg",
-            goodsName:'快乐全家桶',
-            price:80
-          }, {
-            goodsId:5,
-            goodsImg:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566672597068&di=100fe0624bc594ae04b23bb7ab540fec&imgtype=0&src=http%3A%2F%2Fpic.youngt.com%2Fstatic%2Fteam%2F20150528%2F20150528153337_47256.jpg",
-            goodsName:'脆皮炸鸡腿',
-            price:10
-          }, {
-            goodsId:6,
-            goodsImg:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566672597068&di=100fe0624bc594ae04b23bb7ab540fec&imgtype=0&src=http%3A%2F%2Fpic.youngt.com%2Fstatic%2Fteam%2F20150528%2F20150528153337_47256.jpg",
-            goodsName:'魔法鸡块',
-            price:20
-          }, {
-            goodsId:7,
-            goodsImg:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566672597068&di=100fe0624bc594ae04b23bb7ab540fec&imgtype=0&src=http%3A%2F%2Fpic.youngt.com%2Fstatic%2Fteam%2F20150528%2F20150528153337_47256.jpg",
-            goodsName:'可乐大杯',
-            price:10
-          }, {
-            goodsId:8,
-            goodsImg:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566672597068&di=100fe0624bc594ae04b23bb7ab540fec&imgtype=0&src=http%3A%2F%2Fpic.youngt.com%2Fstatic%2Fteam%2F20150528%2F20150528153337_47256.jpg",
-            goodsName:'雪顶咖啡',
-            price:18
-          }, {
-            goodsId:9,
-            goodsImg:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566672597068&di=100fe0624bc594ae04b23bb7ab540fec&imgtype=0&src=http%3A%2F%2Fpic.youngt.com%2Fstatic%2Fteam%2F20150528%2F20150528153337_47256.jpg",
-            goodsName:'大块鸡米花',
-            price:15
-          }, {
-            goodsId:20,
-            goodsImg:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1566672597068&di=100fe0624bc594ae04b23bb7ab540fec&imgtype=0&src=http%3A%2F%2Fpic.youngt.com%2Fstatic%2Fteam%2F20150528%2F20150528153337_47256.jpg",
-            goodsName:'香脆鸡柳',
-            price:17
-          }
-
         ],
+        type1Goods:[
+        ],
+        type2Goods:[
+        ],
+        type3Goods:[
+        ],
+        totalCount:0,
+        totalMoney:0,
+      }
+    },
+    methods:{
+      addtableData(goods){
+        var goodsId = goods.goodsId
+        var isHave = false;
+        for(let i = 0; i<this.tableData.length;i++){
+          if(this.tableData[i].goodsId === goodsId){
+            this.tableData[i].count=this.tableData[i].count+1;
+            isHave = true;
+          }
+        }
+        if(isHave){
+        }else {
+          goods.count= 1;
+          this.tableData.push(goods)
+        }
+        this.getAllMoney();
+      },
+      //删除单个商品
+      delSingleGoods(goods){
+        this.tableData=this.tableData.filter(o => o.goodsId !=goods.goodsId);
+        this.getAllMoney();
+      },
+      //汇总数量和金额
+      getAllMoney(){
+        this.totalCount=0;
+        this.totalMoney=0;
+        if(this.tableData){
+          this.tableData.forEach((element) => {
+            this.totalCount+=element.count;
+            this.totalMoney=this.totalMoney+(element.price*element.count);
+          });
+        }
+      },
+      //删除所有商品
+      delAllGoods() {
+        this.tableData = [];
+        this.totalCount = 0;
+        this.totalMoney = 0;
+      },
+      // 结账
+      checkout() {
+        if (this.totalCount!=0) {
+          this.tableData = [];
+          this.totalCount = 0;
+          this.totalMoney = 0;
+          this.$message({
+            message: '结账成功，感谢你又为店里出了一份力!',
+            type: 'success'
+          });
+        }else{
+          this.$message.error('不能空结。老板了解你急切的心情！');
+        }
+
       }
     },
     mounted:function () {
       /*element組件自定义了一些样式，如需改变样式，需进行dom操作*/
       var orderHeight = document.body.clientHeight;
       document.getElementById("order-list").style.height=orderHeight+'px'
-    }
+    },
+    created(){
+      axios.get('https://www.easy-mock.com/mock/5b8b30dbf032f03c5e71de7f/kuaican/oftenGoods')
+        .then(response=>{
+          this.oftenGoods=response.data;
+        })
+        .catch(error=>{
+          console.log(error);
+          alert('网络错误，不能访问');
+        })
+      //读取分类商品列表
+      axios.get('https://www.easy-mock.com/mock/5b8b30dbf032f03c5e71de7f/kuaican/typeGoods')
+        .then(response=>{
+          console.log(response);
+          //this.oftenGoods=response.data;
+          this.type0Goods=response.data[0];
+          this.type1Goods=response.data[1];
+          this.type2Goods=response.data[2];
+          this.type3Goods=response.data[3];
+
+        })
+        .catch(error=>{
+          console.log(error);
+          alert('网络错误，不能访问');
+        })
+    },
   }
 </script>
 
@@ -222,6 +230,7 @@
   }
   .goods-type{
     clear: both;
+    margin:5px;
   }
   .cookList li{
     list-style: none;
